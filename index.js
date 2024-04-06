ec1aedc91e2610a3ba843781567bfbc2a9296129
Ketllen <161694609+ketllenn@users.noreply.github.com>
2024-04-06 05:43:58 -0300

    Update index.js

--- index.js
+++ index.js
@@ -162,9 +162,3 @@ atualizarLista(participantes)
 }
 
 
-<script type="text/javascript">
-function googleTranslateElementInit() {
-  new google.translate.TranslateElement({pageLanguage: 'pt', includedLanguages: 'en', layout: google.translate.TranslateElement.InlineLayout.SIMPLE}, 'google_translate_element');
-}
-</script>
-<script type="text/javascript" src="//translate.google.com/translate_a/element.js?cb=googleTranslateElementInit"></script>
